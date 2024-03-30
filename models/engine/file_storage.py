0001a2a8dbfe95b37e19ca7b8e5c1b01e4c23653
MARWAHAMED629 <mmero385@gmail.com>
2024-03-30 12:07:50 -0700

    Update DBStorage and FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except Exception:
+        except:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object
+        """
+        objects = list(self.all(cls).values())
+        for object in objects:
+            if object.id == id:
+                return (object)
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        objects = self.all(cls)
+        number = 0
+        for object in objects:
+            number += 1
+        return (number)
