e90587cf60c0d3f4ce1bc8a82857618d8df2d886
MARWAHAMED629 <mmero385@gmail.com>
2024-04-01 23:37:14 +0200

    solving err

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""start point of the Blueprint"""
+"""THE starts point """
 
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
-if app_views:
     from api.v1.views.index import *
     from api.v1.views.states import *
     from api.v1.views.cities import *
