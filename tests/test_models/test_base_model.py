fa41c91def86b5244a8bf249657dd6898b7cd510
root <mmero385@gmail.com>
2024-04-02 19:54:38 -0700

    fix

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,153 +1,20 @@
 #!/usr/bin/python3
-"""
-Unit Test for BaseModel Class
-"""
-import unittest
+"""Test BaseModel for expected behavior and documentation"""
 from datetime import datetime
+import inspect
 import models
-import json
-import os
-
+import pep8 as pycodestyle
+import time
+import unittest
+from unittest import mock
 BaseModel = models.base_model.BaseModel
-storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+module_doc = models.base_model.__doc__
 
 
 class TestBaseModelDocs(unittest.TestCase):
-    """Class for testing BaseModel docs"""
+    """Tests to check the documentation and style of BaseModel class"""
 
     @classmethod
-    def setUpClass(cls):
-        print('\n\n.................................')
-        print('..... Testing Documentation .....')
-        print('.....  For BaseModel Class  .....')
-        print('.................................\n\n')
-
-    def test_doc_file(self):
-        """... documentation for the file"""
-        expected = '\nBaseModel Class of Models Module\n'
-        actual = models.base_model.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_doc_init(self):
-        """... documentation for init function"""
-        expected = 'instantiation of new BaseModel Class'
-        actual = BaseModel.__init__.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_doc_save(self):
-        """... documentation for save function"""
-        expected = 'updates attribute updated_at to current time'
-        actual = BaseModel.save.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_doc_to_json(self):
-        """... documentation for to_json function"""
-        expected = 'returns json representation of self'
-        actual = BaseModel.to_json.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_doc_str(self):
-        """... documentation for to str function"""
-        expected = 'returns string type representation of object instance'
-        actual = BaseModel.__str__.__doc__
-        self.assertEqual(expected, actual)
-
-
-class TestBaseModelInstances(unittest.TestCase):
-    """testing for class instances"""
-
-    @classmethod
-<<<<<<< HEAD
-    def setUpClass(cls):
-        print('\n\n.................................')
-        print('....... Testing Functions .......')
-        print('.....  For BaseModel Class  .....')
-        print('.................................\n\n')
-
-    def setUp(self):
-        """initializes new BaseModel instance for testing"""
-        self.model = BaseModel()
-
-    def test_instantiation(self):
-        """... checks if BaseModel is properly instantiated"""
-        self.assertIsInstance(self.model, BaseModel)
-
-    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
-    def test_to_string(self):
-        """... checks if BaseModel is properly casted to string"""
-        my_str = str(self.model)
-        my_list = ['BaseModel', 'id', 'created_at']
-        actual = 0
-        for sub_str in my_list:
-            if sub_str in my_str:
-                actual += 1
-        self.assertTrue(3 == actual)
-
-    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
-    def test_to_string(self):
-        """... checks if BaseModel is properly casted to string"""
-        my_str = str(self.model)
-        my_list = ['BaseModel', 'id', 'created_at']
-        actual = 0
-        for sub_str in my_list:
-            if sub_str in my_str:
-                actual += 1
-        self.assertTrue(3 == actual)
-
-    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
-    def test_instantiation_no_updated(self):
-        """... should not have updated attribute"""
-        my_str = str(self.model)
-        actual = 0
-        if 'updated_at' in my_str:
-            actual += 1
-        self.assertTrue(0 == actual)
-
-    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
-    def test_save(self):
-        """... save function should add updated_at attribute"""
-        self.model.save()
-        actual = type(self.model.updated_at)
-        expected = type(datetime.now())
-        self.assertEqual(expected, actual)
-
-    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
-    def test_to_json(self):
-        """... to_json should return serializable dict object"""
-        my_model_json = self.model.to_json()
-        actual = 1
-        try:
-            serialized = json.dumps(my_model_json)
-        except:
-            actual = 0
-        self.assertTrue(1 == actual)
-
-    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
-    def test_json_class(self):
-        """... to_json should include class key with value BaseModel"""
-        my_model_json = self.model.to_json()
-        actual = None
-        if my_model_json['__class__']:
-            actual = my_model_json['__class__']
-        expected = 'BaseModel'
-        self.assertEqual(expected, actual)
-
-    def test_name_attribute(self):
-        """... add name attribute"""
-        self.model.name = "Holberton"
-        actual = self.model.name
-        expected = "Holberton"
-        self.assertEqual(expected, actual)
-
-    def test_number_attribute(self):
-        """... add number attribute"""
-        self.model.number = 98
-        actual = self.model.number
-        self.assertTrue(98 == actual)
-
-if __name__ == '__main__':
-    unittest.main
-=======
     def setUpClass(self):
         """Set up for docstring tests"""
         self.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
@@ -292,4 +159,3 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(old_created_at, new_created_at)
         self.assertTrue(mock_storage.new.called)
         self.assertTrue(mock_storage.save.called)
->>>>>>> e56c0be3cb236f4e008a46784f036884691339a2
